02ace31d74d29b266c7fc893f6164229bee1736d
Julius Mwangi <julius.mwangi@moringaschool.com>
2022-05-10 15:24:42 +0300

    commit tree ad4d62a94c2085bb74f94c923b75115ffb842aaa

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "njmwas";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
